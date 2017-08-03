731ea8da12052d1072a394bc95d06a698e1bf502
Ubuntu <ubuntu@ip-172-31-25-14.ap-southeast-1.compute.internal>
2017-08-03 14:48:08 +0000

    Fixed a small issue is getusers function

--- models/users.js
+++ models/users.js
@@ -462,8 +462,8 @@ exports.getUsers = function(req,res){
                         
                     var users = [];
                     //Log the person out and return success
-                    var skip = (pageNo - 1) * perPage;
-                    var limit = perPage;
+                    var skip = (parseInt(pageNo) - 1) * parseInt(perPage);
+                    var limit = parseInt(perPage);
                     collection.find({'email' : {'$exists' : true}},{'salt' : 0,'pass' : 0,'tokens' : 0}).skip(skip).limit(limit).toArray(function(err, results) {
                     res.send(200,{ success : true, 'users' : results });
                     }, function(err) {
